cbee8ee25cd7c0de9abe90f3a2b3e652fa3173cf
Mario Martin moreno <mmartin-@student.42madrid.com>
2023-05-04 19:46:04 +0200

    fix(command/ModeCommand): Mode returns simple reply when +o -o

--- srcs/command/mode/ModeCommand.cpp
+++ srcs/command/mode/ModeCommand.cpp
@@ -1,141 +1,160 @@
 #include "ModeCommand.hpp"
 #include <regex>
 
-ModeCommand::ModeCommand(): Command() {
-}
+ModeCommand::ModeCommand() : Command() {}
 
-ModeCommand::~ModeCommand() {
-}
+ModeCommand::~ModeCommand() {}
 
 ModeCommand::ModeCommand(const ModeCommand &command) {
-	this->operator=(command);
+  this->operator=(command);
 }
 
-ModeCommand	&ModeCommand::operator=(const ModeCommand &command) {
-	this->_args = command._args;
-	return *this;
+ModeCommand &ModeCommand::operator=(const ModeCommand &command) {
+  this->_args = command._args;
+  return *this;
 }
 
-bool	ModeCommand::execute(Client &executor, std::vector<std::string> &args) const {
-	Server	*server = executor.getServer();
+bool ModeCommand::execute(Client &executor,
+                          std::vector<std::string> &args) const {
+  Server *server = executor.getServer();
 
-	if (!executor.getAuthenticated()) {
-		return true;
-	}
+  if (!executor.getAuthenticated()) {
+    return true;
+  }
 
-	if (args.size() < 2) {
-		server->reply(executor, "ERR_NEEDMOREPARAMS", args[0] + " :Not enough parameters");
-		return true;
-	}
+  if (args.size() < 2) {
+    server->reply(executor, "ERR_NEEDMOREPARAMS",
+                  args[0] + " :Not enough parameters");
+    return true;
+  }
 
-	if (args.size() > 2 && !std::regex_match(args[2], std::regex("[\\+\\-opsitnbvw]+"))) {
-		std::string	reply = "";
-		reply.push_back(this->getUnknownMode(args[2]));
+  if (args.size() > 2 &&
+      !std::regex_match(args[2], std::regex("[\\+\\-opsitnbvw]+"))) {
+    std::string reply = "";
+    reply.push_back(this->getUnknownMode(args[2]));
 
-		server->reply(executor, "ERR_UNKNOWNMODE", reply + " :is unknown mode char to me");
-		return true;
-	}
+    server->reply(executor, "ERR_UNKNOWNMODE",
+                  reply + " :is unknown mode char to me");
+    return true;
+  }
 
-	if (args[1].at(0) == '#' || args[1].at(0) == '&') {
-		Channel *channel = channelExists(server, args[1]);
+  if (args[1].at(0) == '#' || args[1].at(0) == '&') {
+    Channel *channel = channelExists(server, args[1]);
 
-		if (!channel) {
-			server->reply(executor, "ERR_NOSUCHCHANNEL", args[1] + " :No such channel");
-			return true;
-		}
+    if (!channel) {
+      server->reply(executor, "ERR_NOSUCHCHANNEL",
+                    args[1] + " :No such channel");
+      return true;
+    }
 
-		if (std::find(executor.getCurrentChannels().begin(), executor.getCurrentChannels().end(), channel) == executor.getCurrentChannels().end()) {
-			server->reply(executor, "ERR_NOTONCHANNEL", args[1] + " :You're not on that channel");
-			return true;
-		}
+    if (std::find(executor.getCurrentChannels().begin(),
+                  executor.getCurrentChannels().end(),
+                  channel) == executor.getCurrentChannels().end()) {
+      server->reply(executor, "ERR_NOTONCHANNEL",
+                    args[1] + " :You're not on that channel");
+      return true;
+    }
 
-		if (args.size() > 2 && !std::regex_match(args[2], std::regex("[\\+\\-ot]+"))) {
-			server->reply(executor, "ERR_UMODEUNKNOWNFLAG", ":Unknown MODE flag");
-			return true;
-		}
+    if (args.size() > 2 &&
+        !std::regex_match(args[2], std::regex("[\\+\\-ot]+"))) {
+      server->reply(executor, "ERR_UMODEUNKNOWNFLAG", ":Unknown MODE flag");
+      return true;
+    }
 
-		if (!channel->hasClientMode(&executor, 'o') && args.size() > 3) {
-			server->reply(executor, "ERR_CHANOPRIVSNEEDED", channel->getName() + " :You're not channel operator");
-			return true;
-		}
+    if (!channel->hasClientMode(&executor, 'o') && args.size() > 3) {
+      server->reply(executor, "ERR_CHANOPRIVSNEEDED",
+                    channel->getName() + " :You're not channel operator");
+      return true;
+    }
 
-		if (args.size() > 2) {
-			bool add = false, changed = false;
-			for (int i = 0; i < args[2].size(); i++) {
-				if (args[2][i] == '+') {
-					add = true;
-					changed = true;
-				} else if (args[2][i] == '-') {
-					add = false;
-					changed = true;
-				} else if (args[2][i] == 't' && changed) {
-					channel->setMode('t', add ? true : false);
-				} else if (args[2][i] == 'o' && changed) {
-					Client	*param = clientExists(server, args[3]);
+    if (args.size() > 2) {
+      bool add = false, changed = false;
+      for (int i = 0; i < args[2].size(); i++) {
+        if (args[2][i] == '+') {
+          add = true;
+          changed = true;
+        } else if (args[2][i] == '-') {
+          add = false;
+          changed = true;
+        } else if (args[2][i] == 't' && changed) {
+          channel->setMode('t', add ? true : false);
+        } else if (args[2][i] == 'o' && changed) {
+          Client *param = clientExists(server, args[3]);
 
-					if (!param) {
-						server->reply(executor, "ERR_NOSUCHNICK", args[3] + " :No such nick/channel");
-						return true;
-					}
+          if (!param) {
+            server->reply(executor, "ERR_NOSUCHNICK",
+                          args[3] + " :No such nick/channel");
+            return true;
+          }
 
-					channel->setClientMode(param, 'o', add ? true : false);
-				}
-			}
-		}
+          channel->setClientMode(param, 'o', add ? true : false);
+          server->simpleReply(channel->getClients(),
+                              "MODE " + channel->getName() + " " +
+                                  (add ? "+" : "-") + "o " + param->getNick());
+        }
+      }
+    }
 
-		server->reply(executor, "RPL_CHANNELMODEIS", channel->getName() + " +" + channel->getModes());
+    server->reply(executor, "RPL_CHANNELMODEIS",
+                  channel->getName() + " +" + channel->getModes());
 
-		return false;
-	}
+    return false;
+  }
 
-	if (args.size() > 2 && !std::regex_match(args[2], std::regex("[\\+\\-iwso]+"))) {
-		server->reply(executor, "ERR_UMODEUNKNOWNFLAG", ":Unknown MODE flag");
-		return true;
-	}
+  if (args.size() > 2 &&
+      !std::regex_match(args[2], std::regex("[\\+\\-iwso]+"))) {
+    server->reply(executor, "ERR_UMODEUNKNOWNFLAG", ":Unknown MODE flag");
+    return true;
+  }
 
-	Client	*client = clientExists(server, args[1]);
-	bool	add = false, changed = false;
+  Client *client = clientExists(server, args[1]);
+  bool add = false, changed = false;
 
-	if (!client) {
-		server->reply(executor, "ERR_NOSUCHNICK", args[1] + " :No such nick/channel");
-		return true;
-	}
+  if (!client) {
+    server->reply(executor, "ERR_NOSUCHNICK",
+                  args[1] + " :No such nick/channel");
+    return true;
+  }
 
-	if (client->getNick() != executor.getNick()) {
-		server->reply(executor, "ERR_USERSDONTMATCH", ":Cant change mode for other users");
-		return true;
-	}
+  if (client->getNick() != executor.getNick()) {
+    server->reply(executor, "ERR_USERSDONTMATCH",
+                  ":Cant change mode for other users");
+    return true;
+  }
 
-	for (int i = 0; i < args[2].size(); i++) {
-		if (args[2][i] == '+') {
-			add = true;
-			changed = true;
-		} else if (args[2][i] == '-') {
-			add = false;
-			changed = true;
-		} else {
-			if (args[2][i] == 'o' && add && changed) {
-				continue;
-			}
+  for (int i = 0; i < args[2].size(); i++) {
+    if (args[2][i] == '+') {
+      add = true;
+      changed = true;
+    } else if (args[2][i] == '-') {
+      add = false;
+      changed = true;
+    } else {
+      if (args[2][i] == 'o' && add && changed) {
+        continue;
+      }
 
-			executor.setMode(args[2][i], (add && changed) ? true : false);
-		}
-	}
+      executor.setMode(args[2][i], (add && changed) ? true : false);
+    }
+  }
 
-	server->reply(executor, "RPL_UMODEIS", ":" + (executor.getModes().empty() ? std::string("") : std::string("+")) + executor.getModes());
+  server->reply(
+      executor, "RPL_UMODEIS",
+      ":" + (executor.getModes().empty() ? std::string("") : std::string("+")) +
+          executor.getModes());
 
-	return false;
+  return false;
 }
 
-char	ModeCommand::getUnknownMode(std::string &modes) const {
-	std::string validModes = "+-opsitnbvw";
+char ModeCommand::getUnknownMode(std::string &modes) const {
+  std::string validModes = "+-opsitnbvw";
 
-	for (int i = 0; i < modes.size(); i++) {
-		int j = validModes.find(modes[i]);
+  for (int i = 0; i < modes.size(); i++) {
+    int j = validModes.find(modes[i]);
 
-		if (j == validModes.npos) {
-			return modes[i];
-		}
-	}
-	return '\0';
+    if (j == validModes.npos) {
+      return modes[i];
+    }
+  }
+  return '\0';
 }
